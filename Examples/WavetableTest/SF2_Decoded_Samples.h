16ecb5cb05ec796b304663e5eb06867633136b23
Ryan Mellmer <Ryan Mellmer>
2017-01-30 12:54:45 -0800

    Adding new wavetable test

---
+++ Examples/WavetableTest/SF2_Decoded_Samples.h
@@ -0,0 +1,8 @@
+extern const unsigned int sample[20480];
+struct sample_info {
+	const int ORIGINAL_PITCH = 60;
+	const int SAMPLE_RATE = 44100;
+	const bool IS_MONO = 1;
+	const int LOOP_START = 8;
+	const int LOOP_END = 40943;
+};
