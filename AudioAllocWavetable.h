762ed2510997f7c05d17b5d1b2269e244b7f0a83
Connor Delaplane <octarine92@gmail.com>
2017-03-03 18:32:57 -0800

    Fixed indentation issues

--- AudioAllocWavetable.h
+++ AudioAllocWavetable.h
@@ -17,7 +17,7 @@ public:
 	/**
 	 * Class constructor.
 	 */
-    AudioAllocWavetable(void) 
+	AudioAllocWavetable(void)
 		: voices(NULL)
 		, num_voices(0)
 		, amplitude(1.0)
@@ -34,7 +34,7 @@ public:
 	 * @param num_samples the length of 'samples'.
 	 * @param amp the amplitude for each voice (between 0.0 and 1.0).
 	 */
-    void init(AudioSynthWavetable* voices, uint8_t num_voices, sample_data* wavetable, int num_samples, float amp);
+	void init(AudioSynthWavetable* voices, uint8_t num_voices, sample_data* wavetable, int num_samples, float amp);
 	
 	/**
 	 * @brief Set the sample data for each voice.
@@ -42,14 +42,14 @@ public:
 	 * @param samples an array of samples from a decoded SoundFont file.
 	 * @param num_samples the length of 'samples'.
 	 */
-    void setSamples(sample_data* samples, int num_samples);
+	void setSamples(sample_data* samples, int num_samples);
 	
 	/**
 	 * @brief Set the amplitude for each voice.
 	 *
 	 * @param amp A value between 0.0 and 1.0.
 	 */
-    void setAmplitude(float amp);
+	void setAmplitude(float amp);
 	
 	/**
 	 * @brief Find an available voice and instruct it to play at the specified frequency.
@@ -59,7 +59,7 @@ public:
 	 *
 	 * @param freq the desired frequency for playback (between 0 and the board-specific sample rate).
 	 */
-    void playFreq(float freq);
+	void playFreq(float freq);
 	
 	/**
 	 * @brief Find an available voice and instruct it to play the specified note.
@@ -69,7 +69,7 @@ public:
 	 *
 	 * @param note the desired note to be played (between 0 and 127).
 	 */
-    void playNote(int note);
+	void playNote(int note);
 	
 	/**
 	 * @brief If a voice is playing at the specified frequency, stop it.
@@ -77,7 +77,7 @@ public:
 	 *
 	 * @param freq the desired frequency to stop (between 0 and the board-specific sample rate).
 	 */
-    void stopFreq(float freq);
+	void stopFreq(float freq);
 	
 	/**
 	 * @brief If a voice is playing the specified note, stop it.
@@ -85,23 +85,23 @@ public:
 	 *
 	 * @param note the desired note to stop (between 0 and 127).
 	 */
-    void stopNote(int note);
+	void stopNote(int note);
 	
 	/**
 	 * @return the number of voices currently playing (not idle).
 	 */
-    uint8_t voicesPlaying(void);
+	uint8_t voicesPlaying(void);
 	
 	/**
 	 * @return the current amplitude settings.
 	 */
-    float getAmplitude() { return amplitude; }
+	float getAmplitude() { return amplitude; }
 	
 	/**
 	 * @param index the index of a voice (between 0 and the number of voices allocated minus 1).
 	 * @return the frequency that voice is playing (or -1 if voice is idle).
 	 */
-    float getFrequency(uint8_t index) { return freqTrack[index].freq; }
+	float getFrequency(uint8_t index) { return freqTrack[index].freq; }
     
 private:
 	struct freqTrack_t
@@ -112,9 +112,9 @@ private:
 	
 	float noteToFreq(int note);
 	
-    AudioSynthWavetable* voices;
-    uint8_t num_voices;
-    freqTrack_t freqTrack[256];
-    float amplitude;
+	AudioSynthWavetable* voices;
+	uint8_t num_voices;
+	freqTrack_t freqTrack[256];
+	float amplitude;
 };
 #endif
