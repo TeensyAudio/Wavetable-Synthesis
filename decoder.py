070f774a41a8aa0521f7c22290e773b905af0236
Xuan Tang <ytang@pdx.edu>
2017-03-05 23:54:50 -0800

    added vib and mod extraction to decoder.py

--- decoder.py
+++ decoder.py
@@ -262,7 +262,6 @@ def gen_sample_meta_data_string(bag, global_bag, sample_num, instrument_name, ke
         "\t\tuint32_t({DECAY_ENV}*SAMPLES_PER_MSEC/8.0+0.5),\t//DECAY_COUNT\n" \
         "\t\tuint32_t({RELEASE_ENV}*SAMPLES_PER_MSEC/8.0+0.5),\t//RELEASE_COUNT\n" \
         "\t\tint32_t({SUSTAIN_FRAC}*UNITY_GAIN),\t//SUSTAIN_MULT\n" \
-
         "\t\tuint32_t({VIB_DELAY_ENV} * SAMPLES_PER_MSEC / (2 * LFO_PERIOD)), \t// VIBRATO_DELAY\n" \
         "\t\tuint32_t({VIB_INC_ENV} * LFO_PERIOD * (UINT32_MAX / AUDIO_SAMPLE_RATE_EXACT)), // VIBRATO_INCREMENT\n" \
         "\t\tCENTS_SHIFT(-8) - 1.0, // VIBRATO_PITCH_COEFFICIENT_INITIAL\n" \
@@ -321,10 +320,10 @@ def gen_sample_meta_data_string(bag, global_bag, sample_num, instrument_name, ke
         "DECAY_ENV": bag.volume_envelope_decay if bag.volume_envelope_decay else global_bag.volume_envelope_decay,
         "SUSTAIN_FRAC": sustain_frac,
         "RELEASE_ENV": bag.volume_envelope_release if bag.volume_envelope_release else global_bag.volume_envelope_release,
-        "VIB_DELAY_ENV": #--------------#,
-        "VIB_INC_ENV":
-        "MOD_DELAY_ENV":
-        "MOD_INC_ENV":
+        "VIB_DELAY_ENV": bag.gens[23].cents if 23 in bag.gens else global_bag.gens[23].cents if 23 in global_bag.gens else None,
+        "VIB_INC_ENV": bag.gens[24].cents if 24 in bag.gens else global_bag.gens[24].cents if 24 in global_bag.gens else None,
+        "MOD_DELAY_ENV": bag.gens[21].cents if 21 in bag.gens else global_bag.gens[21].cents if 21 in global_bag.gens else None,
+        "MOD_INC_ENV": bag.gens[22].cents if 22 in bag.gens else global_bag.gens[22].cents if 22 in global_bag.gens else None,
     }
     env_vals = {k: int(env_vals[k] * 1000) if env_vals[k] else 0 for k in env_vals}
 
