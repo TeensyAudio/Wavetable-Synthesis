b35f12aa01530d1894479406c9b19008bff3f9a3
aidakeifer <aidakeifer@gmail.com>
2017-03-12 13:07:57 -0700

    formatting changes

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -3,23 +3,34 @@ var NAVTREE =
   [ "Teensy Wavetable Synthesis", "index.html", [
     [ "Wavetable-Synthesis", "index.html", null ],
     [ "Examples", "md_additional_pages_examples.html", null ],
-    [ "Getting started", "md_additional_pages_gettingStarted.html", null ],
-    [ "SoundFont Decoder User-Guide", "md_additional_pages_soundfontDecoder.html", [
-      [ "1-0 About", "md_additional_pages_soundfontDecoder.html#about", null ],
-      [ "2-0 Using the Decoder with the GUI", "md_additional_pages_soundfontDecoder.html#decoderGUI", [
-        [ "2-1 GUI Layout", "md_additional_pages_soundfontDecoder.html#guiLayout", null ],
-        [ "2-2 Decoding a Sample", "md_additional_pages_soundfontDecoder.html#decodesampleGUI", null ]
+    [ "Getting started", "md_additional_pages_getting_started.html", [
+      [ "What is the Audio Synth Wavetable library?", "md_additional_pages_getting_started.html#WhatIs", null ],
+      [ "How do I use the Audio Synth Wavetable library?", "md_additional_pages_getting_started.html#howtoUse", null ],
+      [ "Decoding Soundfont Files", "md_additional_pages_getting_started.html#decoder", null ],
+      [ "Including a Soundfont File in an Arduino Sketch", "md_additional_pages_getting_started.html#including", null ],
+      [ "Locating the instrument_data Within the Decoded Files", "md_additional_pages_getting_started.html#locating", null ],
+      [ "Setting up an AudioSynthWavetable Object", "md_additional_pages_getting_started.html#settingObject", [
+        [ "Audio Connections", "md_additional_pages_getting_started.html#AudioConnectios", null ],
+        [ "Setting an Object's Instrument", "md_additional_pages_getting_started.html#settingInst", null ]
       ] ],
-      [ "3-0 Using the Decoder from the Command Line", "md_additional_pages_soundfontDecoder.html#decodercommandline", [
-        [ "3-1 Decoding a Sample", "md_additional_pages_soundfontDecoder.html#decodesamplecl", null ],
-        [ "3-2 Command Line Options", "md_additional_pages_soundfontDecoder.html#CLoptions", null ]
+      [ "A very simple first program", "md_additional_pages_getting_started.html#firstProg", null ]
+    ] ],
+    [ "SoundFont Decoder User-Guide", "md_additional_pages_soundfont_decoder.html", [
+      [ "1-0 About", "md_additional_pages_soundfont_decoder.html#about", null ],
+      [ "2-0 Using the Decoder with the GUI", "md_additional_pages_soundfont_decoder.html#decoderGUI", [
+        [ "2-1 GUI Layout", "md_additional_pages_soundfont_decoder.html#guiLayout", null ],
+        [ "2-2 Decoding a Sample", "md_additional_pages_soundfont_decoder.html#decodesampleGUI", null ]
       ] ],
-      [ "4-0 Decoder Output", "md_additional_pages_soundfontDecoder.html#decoderoutput", [
-        [ "4-1 .cpp file", "md_additional_pages_soundfontDecoder.html#cppfile", null ],
-        [ "4-2 .h file", "md_additional_pages_soundfontDecoder.html#hfile", null ]
+      [ "3-0 Using the Decoder from the Command Line", "md_additional_pages_soundfont_decoder.html#decodercommandline", [
+        [ "3-1 Decoding a Sample", "md_additional_pages_soundfont_decoder.html#decodesamplecl", null ],
+        [ "3-2 Command Line Options", "md_additional_pages_soundfont_decoder.html#CLoptions", null ]
+      ] ],
+      [ "4-0 Decoder Output", "md_additional_pages_soundfont_decoder.html#decoderoutput", [
+        [ "4-1 .cpp file", "md_additional_pages_soundfont_decoder.html#cppfile", null ],
+        [ "4-2 .h file", "md_additional_pages_soundfont_decoder.html#hfile", null ]
       ] ]
     ] ],
-    [ "Classes", "annotated.html", [
+    [ "AudioSynthWavetable Class", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
       [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
