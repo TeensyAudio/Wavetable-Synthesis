cf534c34b33ddd480e8f87e1dfd049a214482ee3
Ryan Mellmer <ryanmellmer@gmail.com>
2017-03-16 19:21:50 -0700

    Including example sketch in the examples folder

---
+++ Examples/SimpleWavetable/BasicFlute1_samples.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <AudioStream.h>
+#include <AudioSynthWavetable.h>
+
+extern const sample_data BasicFlute1_samples[1];
+const uint8_t BasicFlute1_ranges[] = {127,};
+
+const instrument_data BasicFlute1 = {1, BasicFlute1_ranges, BasicFlute1_samples };
+
+
+extern const uint32_t sample_0_BasicFlute1_BreathyFluteC2[28544];
